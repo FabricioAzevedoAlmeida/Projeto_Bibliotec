81dde6ac40c2c1cba08eeeb3fd860eae36f61d34
FABRICIO AZEVEDO ALMEIDA <48748244805@info.local>
2026-09-04 17:26:39 -0300

    tela de login

---
+++ login/login.js
@@ -0,0 +1,12 @@
+// var nome = "Késsia" -> Não usa 
+// const nome = "Késsia" -> Não mudar a variavel
+//let nome = "Késsia"; -> Mudar a variavel
+
+//console.log(nome);
+
+const campoSenha = document.getElementById("senha")
+const btnSenha = document.querySelector("#mostrar-senha")
+
+btnSenha.addEventListener("click", function() {
+    alert("Cliquei no botão!");
+})
